79f68e4e96a073d1af8c2310a4f5a16e12010558
LIUXXXUYANG <32128344+LIUXXXUYANG@users.noreply.github.com>
2017-09-25 19:07:54 +0100

    Add files via upload
    use dplyr to get a summary of the data

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -1,5 +1,7 @@
+library(dplyr)
 crimedata <- read.csv("Crime_Data_2010_2017.csv")
-
+crimedata<-tbl_df(crimedata)
+glimpse(crimedata)
 str(crimedata)
 
 crimedata$Date.Reported_C <- as.Date(crimedata$Date.Reported, format=("%m/%d/%Y"))
